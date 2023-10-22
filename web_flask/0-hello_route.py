a5687de509c5112bd4575504f5fdda6d6a3fa320
root <root@stan>
2023-10-22 22:47:41 +0300

    hello_route.py 2day

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" Script that starts a Flask web application """
 from flask import Flask
 
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """ Function called with / route """
+    """print web"""
     return 'Hello HBNB!'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
