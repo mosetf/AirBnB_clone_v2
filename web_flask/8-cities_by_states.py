263d1b04e7d0173183c2c812484215e5126bf3b2
root <root@stan>
2023-10-22 23:34:59 +0300

    cities_by_states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def closedb(exc):
+    """ to close a database session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html',
-                           Table="States",
-                           states=states)
+@app.route('/cities_by_states')
+def states_list():
+    """ /states_list route """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
