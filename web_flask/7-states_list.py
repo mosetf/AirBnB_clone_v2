a3a85d473130614fa1339c3539cecf21578eef96
root <root@stan>
2023-10-22 23:19:33 +0300

    states_list7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/states_list: HTML page with a list of all State objects in DBStorage.
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def close_db(exc):
+    """close the current session of sqlalchemist"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States",
-                           items=dict_to_html)
+@app.route('/states_list')
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
